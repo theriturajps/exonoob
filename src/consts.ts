141d3893e476b0fa20dbfa011497a090e268cde8
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-02-24 19:12:55 +0530

    Update site title and author name for consistency and branding

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Exo Noob's Blog",
+  TITLE: "ExoNoob",
   DESCRIPTION: "Exo Noob is your ultimate resource for beginner-friendly blogging and tech advice. Whether you're just starting your blog or looking to enhance your online presence, we offer clear, actionable guides and smart strategies to help you succeed. From choosing the right blogging niche to mastering SEO and monetization, our easy-to-follow tips and expert insights will transform your blogging journey. Explore top-tier content and Start your path to blogging success with exonoob.in today!",
-  AUTHOR: 'Ritu Raj',
+  AUTHOR: 'Ritu Raj Pratap Singh',
 }
 
 // Work Page
