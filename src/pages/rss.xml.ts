ed35423198997401a293e81352ccdc64f4a134e6
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-03-04 21:36:29 +0530

    Refactor canonical URL handling and update blog slug paths to remove trailing slashes

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,7 +7,7 @@ type Context = {
 }
 
 export async function GET(context: Context) {
-	const posts = await getCollection("blog")
+  const posts = await getCollection("blog")
   const projects = await getCollection("projects")
 
   const items = [...posts, ...projects]
@@ -23,8 +23,8 @@ export async function GET(context: Context) {
       description: item.data.summary,
       pubDate: item.data.date,
       link: item.slug.startsWith("blog")
-        ? `/blog/${item.slug}/`
-        : `/projects/${item.slug}/`,
+        ? `/blog/${item.slug}`
+        : `/projects/${item.slug}`,
     })),
   })
 }
