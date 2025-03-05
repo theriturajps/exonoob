657732c36b7a0b6e669038dad09f0e8cb9cf7392
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-03-05 10:03:27 +0530

    Remove trailing slash enforcement from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://exonoob.in/",
-  trailingSlash: "always",
   integrations: [
     mdx(),
     solidJs(),
